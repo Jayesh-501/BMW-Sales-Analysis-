6ab3cdce87e87e2ff09cac7bd1b6a304c0bebba0
Jayesh Kados <jayeshkados@gmail.com>
2025-10-07 14:52:35 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bmw_sales (
+    Model VARCHAR(50),
+    Year INT,
+    Region VARCHAR(50),
+    Color VARCHAR(30),
+    Fuel_Type VARCHAR(30),
+    Transmission VARCHAR(30),
+    Engine_Size_L DECIMAL(3,1),
+    Mileage_KM INT,
+    Price_USD INT,
+    Sales_Volume INT,
+    Sales_Classification VARCHAR(10)
+);
